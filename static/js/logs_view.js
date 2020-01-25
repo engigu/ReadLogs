4c119b1fce293fc14f7c0e13196f13a19ca5a223
EngiGu <sayheya@qq.com>
2020-01-25 16:11:25 +0800

    update

--- static/js/logs_view.js
+++ static/js/logs_view.js
@@ -8,7 +8,7 @@ $(document).ready(function () {
         // getData(filepath, true);
         // console.log(filepath)
         fp_path = filepath;
-        socket.emit('client', {'fp_path': filepath});
+        socket.emit('client', { 'fp_path': filepath });
         $("#terminal").html(""); // 切换清空显示区域
     });
 
@@ -20,24 +20,28 @@ $(document).ready(function () {
         if (res.path === fp_path) {
             var top_info = res.text;
             var current_length = $('#terminal li').length;
-            var limit_length = $('#terminal').attr("num"); 
+            var limit_length = $('#terminal').attr("num");
             limit_length = Number(limit_length);
             // console.log(current_length, limit_length);
-            if (current_length >= limit_length){
-			    $("#terminal li:first").remove();
+            if (current_length >= limit_length) {
+                $("#terminal li:first").remove();
             }
             $("#terminal").append("<li> None </pli>");
             $("#terminal li:last").text(top_info);
-            
-            window.scrollTo(0, document.documentElement.clientHeight);
+
 
         }
 
     });
 
-    $(window).bind('onbeforeunload', function () {    // 离开页面前关闭tail
-            socket.emit('leavepage', {'_type': _type});
+    setInterval(function () {
+        // 一直滑动到最下面
+        window.scrollTo(0, document.documentElement.clientHeight);
+    }, 1000);
 
-        }
+    $(window).bind('onbeforeunload', function () {    // 离开页面前关闭tail
+        socket.emit('leavepage', { '_type': _type });
+
+    }
     );
 });
